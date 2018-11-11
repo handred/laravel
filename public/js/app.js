431003c6fff2b5409ee12101e894e1a07eb97d92
Your Name <you@example.com>
2018-11-11 14:59:20 +0500

    Merge origin/master

--- public/js/app.js
+++ public/js/app.js
@@ -47389,12 +47389,12 @@ var staticRenderFns = [
         _c("div", { staticClass: "col-md-8" }, [
           _c("div", { staticClass: "card card-default" }, [
             _c("div", { staticClass: "card-header" }, [
-              _vm._v("Example Component")
+              _vm._v("Example Component Header")
             ]),
             _vm._v(" "),
             _c("div", { staticClass: "card-body" }, [
               _vm._v(
-                "\n                    I'm an example component.\n                "
+                "\n                    Vue compoytyn Content\n                "
               )
             ])
           ])
