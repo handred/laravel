3e8fe076302542a3bdd430ba1bfdc20512549d6e
Your Name <you@example.com>
2018-11-18 23:08:53 +0500

    Merge origin/master

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ var io = require('socket.io')(http);
 
 var Redis = require('ioredis');
 
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+
+    socket.on('message', function (msg) {
+        console.log('message: ' + msg);
+        io.emit('message', msg);
+    });
+});
+
 var redis = new Redis();
 redis.subscribe('chartevent');
 redis.on('message', function (channel, message) {
